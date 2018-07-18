c24d6332b1f4deb9f9bea9d5f6bfd7cad5ca37c3
wang.andy32@gmail.com <wang.andy32@gmail.com>
2018-07-18 01:21:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(mus,ins){
+  var arr= [];
+  for(var i=0;i<mus.length;i++){
+    arr.push(mus[i] + " plays " + ins[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  var i=0;
+  while(i<facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts;
+}
+
